291ca138b6c4cd3b480ad1ab540a7483af7b59ab
Alexander Solovioff <asoloviev@openwaygroup.com>
2021-04-19 01:44:46 +0300

    pseudoclasses

---
+++ samples/pseudoclasses/src/main/java/wn/pseudoclasses/Pseudo.java
@@ -0,0 +1,16 @@
+package wn.pseudoclasses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Alexander A. Solovioff
+ * Date: 19.04.2021
+ * Time: 1:40 AM
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Pseudo {
+}
