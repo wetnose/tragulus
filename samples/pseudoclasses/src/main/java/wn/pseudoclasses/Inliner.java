20cac0c246cfe60a9a4b88be7658643ac225e6c8
Alexander Solovioff <asoloviev@openintegration.inc>
2022-11-23 02:37:11 +0300

    Inliner ref, FuncCall

--- samples/pseudoclasses/src/main/java/wn/pseudoclasses/Inliner.java
+++ samples/pseudoclasses/src/main/java/wn/pseudoclasses/Inliner.java
@@ -1,26 +1,23 @@
 package wn.pseudoclasses;
 
-import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.ExpressionTree;
-import com.sun.source.tree.Tree;
+import com.sun.source.tree.MethodInvocationTree;
 import com.sun.source.tree.TypeCastTree;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
+import com.sun.source.util.TreePathScanner;
 import wn.pseudoclasses.Pseudos.Extension;
+import wn.pseudoclasses.Pseudos.Method;
 import wn.pseudoclasses.Pseudos.PseudoType;
 import wn.tragulus.Editors;
-import wn.tragulus.JavacUtils.TreeWalker;
 import wn.tragulus.ProcessingHelper;
 import wn.tragulus.TreeAssembler;
 
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Types;
-import java.util.function.Consumer;
+import java.util.ArrayList;
 
 import static wn.pseudoclasses.Pseudos.Err.CANNOT_CAST;
-import static wn.tragulus.JavacUtils.walkOver;
 
 /**
  * Alexander A. Solovioff
@@ -32,7 +29,7 @@ class Inliner {
     void inline(PseudoType type) {
 
         if (type instanceof Extension) {
-            //System.out.println("---- test " + type.elem);
+
             Pseudos pseudos = type.pseudos();
             Types types = pseudos.types;
             ProcessingHelper helper = pseudos.helper;
@@ -40,59 +37,53 @@ class Inliner {
 
             TypeMirror find = type.elem.asType();
             TypeMirror replace = ((Extension) type).wrappedType;
-            class Walker implements Consumer<TreeWalker> {
+
+            TreePathScanner<Invocations,Void> scanner = new TreePathScanner<>() {
+
                 @Override
-                public void accept(TreeWalker walker) {
-                    TreePath path = walker.path();
-                    Tree node = path.getLeaf();
-                    System.out.println(node + " " + node.getKind());
-                    switch (node.getKind()) {
-                        case VARIABLE:
-                            VariableTree var = (VariableTree) node;
-                            if (helper.typeOf(path, var.getType()) == find) {
-                                Editors.setType(var, asm.type(replace).asExpr());
-                            }
-                            break;
-                        case TYPE_CAST:
-                            TypeCastTree cast = (TypeCastTree) node;
-                            if (helper.typeOf(path, cast.getType()) == find) {
-                                ExpressionTree expr = cast.getExpression();
-                                if (types.isAssignable(helper.typeOf(path, expr), replace)) {
-                                    pseudos.suppressDiagnostics(CANNOT_CAST, expr);
-                                    Editors.setType(cast, asm.type(replace).asExpr());
-                                }
-                            }
-                            System.out.println(cast);
-                            break;
-
-//                        case IDENTIFIER:
-//                            Element element = helper.asElement(path);
-//                            System.out.print(node);
-//                            if (element != null) {
-//                                Element enc = helper.asElement(path.getParentPath());
-//                                if (enc != null) System.out.print(" " + enc + " " + enc.getKind());
-//                            }
-//                            System.out.println();
-//                            break;
+                public Invocations visitVariable(VariableTree node, Void unused) {
+                    super.visitVariable(node, unused);
+                    TreePath path = getCurrentPath();
+                    if (helper.typeOf(path, node.getType()) == find) {
+                        Editors.setType(node, asm.type(replace).asExpr());
                     }
+                    return null;
                 }
-            }
 
-            Walker walker = new Walker();
-            type.units.forEach(unit -> {
-                walkOver(unit, walker);
-
-                unit.getTypeDecls().forEach(tree -> {
-                    ((ClassTree) tree).getMembers().forEach(member -> {
-                        if (member.getKind() != Tree.Kind.VARIABLE) return;
-                        VariableElement var = helper.asElement(unit, member);
-                        TypeElement varType = helper.asElement(var.asType());
-                        if (type.elem == varType) {
-                            Editors.setType((VariableTree) member, asm.type(replace).asExpr());
+                @Override
+                public Invocations visitTypeCast(TypeCastTree node, Void unused) {
+                    super.visitTypeCast(node, unused);
+                    TreePath path = getCurrentPath();
+                    if (helper.typeOf(path, node.getType()) == find) {
+                        ExpressionTree expr = node.getExpression();
+                        if (types.isAssignable(helper.typeOf(path, expr), replace)) {
+                            pseudos.suppressDiagnostics(CANNOT_CAST, expr);
+                            Editors.setType(node, asm.type(replace).asExpr());
                         }
-                    });
-                });
+                    }
+                    return null;
+                }
+            };
+
+            type.units.forEach(unit -> {
+                scanner.scan(unit, null);
             });
         }
+    }
+
+
+    private static class Invocation {
+        final MethodInvocationTree tree;
+        final Method method;
+
+        Invocation(MethodInvocationTree tree, Method method) {
+            this.tree = tree;
+            this.method = method;
+        }
+    }
+
+
+    private static class Invocations extends ArrayList<Invocation> {
+
     }
 }
---
+++ samples/pseudoclasses/src/test/resources/FuncCall.java
@@ -0,0 +1,8 @@
+public class FuncCall {
+
+    public static void main(String[] args) {
+        IntAnatomy0 an = (IntAnatomy0) (args.length + 15);
+        System.out.println(an);
+        //System.out.println(an.getByte(0));
+    }
+}
